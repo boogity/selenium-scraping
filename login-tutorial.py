9da37c0a671eead4dcae71c44b9f17cdc2e263c3
boogity <harumph505@gmail.com>
2023-06-19 15:17:07 -0500

    Testing additional functionality ahead of regaining access to the RV55

--- login-tutorial.py
+++ login-tutorial.py
@@ -9,15 +9,21 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+# Only need this to access OS environment variables for secrets
+import os
 
 # Github credentials
 username = "boogity"
-password = ""
+# ->$ export GITHUB_PASSWORD='<your github password>'
+password = os.getenv('GITHUB_PASSWORD')
 
 # initialize the Chrome driver
 driver = webdriver.Chrome()
 
-# Get the github login form
+###############################################################################
+#                               LOGIN TO GITHUB
+###############################################################################
 driver.get("https://github.com/login")
 # Find username/password field, input relevant values
 driver.find_element(By.ID, "login_field").send_keys(username)
@@ -40,5 +46,27 @@ if any(error_message in e.text for e in errors):
     print("[!] Login failed")
 else:
     print("[+] Login successful")
+
+###############################################################################
+#                         ACCESS PRIVILEGED PAGE
+###############################################################################
+
+print("You'll want to use driver.page_source to access to HTML for the page.")
+print("You should use BS4 html parser in conjunction with the raw html of driver.page_source")
+
+
+# Testing whether soup is needed at all, looks like the formatting is much nicer in soup
+# Do we care about the format if we're just looking to grep specific values though?
+driver.get("https://github.com/settings/keys")
+with open("sel.html", "a") as f:
+    print(driver.page_source, file=f)
+    print("Output raw html from driver.page_source to ./sel.html")
+
+with open("soup.html", "a") as f:
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    print(soup.prettify(), file=f)
+    print("Output the prettified HTML from BS4's parsing of the raw HTML to ./soup.html")
+
+
 
 driver.quit()
